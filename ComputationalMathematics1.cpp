03e74f398f86583d449bd2ed12c5aadc51ed8f37
delokoseni <107359196+delokoseni@users.noreply.github.com>
2024-02-01 00:47:00 +0700

    Сделана обработка первого столбца матрицы

--- ComputationalMathematics1.cpp
+++ ComputationalMathematics1.cpp
@@ -1,44 +1,67 @@
 ﻿#include <iostream>
 #include <fstream>
 
+const int MatrixDimension = 4;
+void MatrixOutput(double Matrix[MatrixDimension][MatrixDimension]);
+
 using namespace std;
 
 int main()
 {
-    double MatrixA[4][4], MatrixB[4];
+    int i, j;
+    double MatrixA[MatrixDimension][MatrixDimension], MatrixB[MatrixDimension];
     string FileName;
-    cout << "Input name of file for matrix A: ";
+    /*cout << "Input name of file for matrix A: ";
     cin >> FileName;
-    ifstream InputFileA(FileName);
+    ifstream InputFileA(FileName);*/
+    ifstream InputFileA("A.txt");
     if (InputFileA.is_open())
     {
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 4; j++)
+        for (i = 0; i < MatrixDimension; i++)
+            for (j = 0; j < MatrixDimension; j++)
                 InputFileA >> MatrixA[i][j];
         InputFileA.close();
     }
     else
         cout << "Error! File for matrix A isn`t open!";
-    cout << "Input name of file for matrix B: ";
+    /*cout << "Input name of file for matrix B: ";
     cin >> FileName;
-    ifstream InputFileB(FileName);
+    ifstream InputFileB(FileName);*/
+    ifstream InputFileB("B.txt");
     if (InputFileB.is_open())
     {
-        for (int i = 0; i < 4; i++)
+        for (i = 0; i < MatrixDimension; i++)
             InputFileB >> MatrixB[i];
         InputFileB.close();
     }
     else
         cout << "Error! File for matrix B isn`t open!";
     cout << endl << "Matrix A: " << endl;
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++)
-            cout << MatrixA[i][j] << "\t";
-        cout << endl;
-    }
+    MatrixOutput(MatrixA);
     cout << endl << "Matrix B: " << endl;
-    for (int i = 0; i < 4; i++)
+    for (i = 0; i < MatrixDimension; i++)
             cout << MatrixB[i] << endl;
+    int buffer;
+    for (i = 0; i < 1; i++) {
+        for (j = 1; j < MatrixDimension; j++)
+        {
+            buffer = MatrixA[j][0];
+            for (int k = 0; k < MatrixDimension; k++)
+            {
+                MatrixA[j][k] -= MatrixA[i][k] * buffer / MatrixA[i][i];
+            }
+        }
+    }
+    cout << endl << "Matrix A: " << endl;
+    MatrixOutput(MatrixA);
     return 0;
 }
 
+void MatrixOutput(double Matrix[MatrixDimension][MatrixDimension]) 
+{
+    for (int i = 0; i < MatrixDimension; i++) {
+        for (int j = 0; j < MatrixDimension; j++)
+            cout << Matrix[i][j] << "\t";
+        cout << endl;
+    }
+}
