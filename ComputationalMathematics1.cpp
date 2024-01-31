85730d23bf027fee648bf45f5e670e0227b11427
delokoseni <107359196+delokoseni@users.noreply.github.com>
2024-01-31 23:21:01 +0700

    Добавлен ввод матриц из текстового файла

--- ComputationalMathematics1.cpp
+++ ComputationalMathematics1.cpp
@@ -1,20 +1,44 @@
-﻿// ComputationalMathematics1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <fstream>
 
-#include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double MatrixA[4][4], MatrixB[4];
+    string FileName;
+    cout << "Input name of file for matrix A: ";
+    cin >> FileName;
+    ifstream InputFileA(FileName);
+    if (InputFileA.is_open())
+    {
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                InputFileA >> MatrixA[i][j];
+        InputFileA.close();
+    }
+    else
+        cout << "Error! File for matrix A isn`t open!";
+    cout << "Input name of file for matrix B: ";
+    cin >> FileName;
+    ifstream InputFileB(FileName);
+    if (InputFileB.is_open())
+    {
+        for (int i = 0; i < 4; i++)
+            InputFileB >> MatrixB[i];
+        InputFileB.close();
+    }
+    else
+        cout << "Error! File for matrix B isn`t open!";
+    cout << endl << "Matrix A: " << endl;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++)
+            cout << MatrixA[i][j] << "\t";
+        cout << endl;
+    }
+    cout << endl << "Matrix B: " << endl;
+    for (int i = 0; i < 4; i++)
+            cout << MatrixB[i] << endl;
+    return 0;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
