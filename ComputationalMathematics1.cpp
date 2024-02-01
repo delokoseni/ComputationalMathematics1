8672e19b8ff8baa31e34e8af1b880a8199e1acd1
delokoseni <107359196+delokoseni@users.noreply.github.com>
2024-02-01 15:08:26 +0700

    Получено плохое, но верное решение

--- ComputationalMathematics1.cpp
+++ ComputationalMathematics1.cpp
@@ -2,14 +2,14 @@
 #include <fstream>
 
 const int MatrixDimension = 4;
-void MatrixOutput(double Matrix[MatrixDimension][MatrixDimension]);
+void MatrixOutput(float Matrix[MatrixDimension][MatrixDimension]);
 
 using namespace std;
 
 int main()
 {
     int i, j;
-    double MatrixA[MatrixDimension][MatrixDimension], MatrixB[MatrixDimension];
+    float MatrixA[MatrixDimension][MatrixDimension], MatrixB[MatrixDimension];
     string FileName;
     /*cout << "Input name of file for matrix A: ";
     cin >> FileName;
@@ -40,16 +40,54 @@ int main()
     MatrixOutput(MatrixA);
     cout << endl << "Matrix B: " << endl;
     for (i = 0; i < MatrixDimension; i++)
-            cout << MatrixB[i] << endl;
-    int buffer;
-    for (i = 0; i < 1; i++) {
-        for (j = 1; j < MatrixDimension; j++)
+        cout << MatrixB[i] << endl;
+    float buffer;
+
+    int n = 0;
+
+    for (j = 1; j < MatrixDimension; j++)
+    {
+        n = 0;
+        while (MatrixA[j][n] == 0) {
+            n++;
+        }
+        buffer = MatrixA[j][n];
+        for (int k = 0; k < MatrixDimension; k++)
         {
-            buffer = MatrixA[j][0];
-            for (int k = 0; k < MatrixDimension; k++)
-            {
-                MatrixA[j][k] -= MatrixA[i][k] * buffer / MatrixA[i][i];
-            }
+            MatrixA[j][k] -= MatrixA[0][k] * buffer / MatrixA[0][0];
+        }
+    }
+    cout << endl << "Matrix A: " << endl;
+    MatrixOutput(MatrixA);
+
+
+    for (j = 2; j < MatrixDimension; j++)
+    {
+        buffer = MatrixA[j][1];
+        //cout << endl << "buffer[" << j << "][" << n << "]: " << buffer << endl;
+        for (int k = 0; k < MatrixDimension; k++)
+        {
+            cout << MatrixA[j][k] << " - " << MatrixA[1][k] << " * " << buffer << " / " << MatrixA[1][1] << " = " << " ";
+            MatrixA[j][k] -= MatrixA[1][k] * buffer / MatrixA[1][1];
+            cout << MatrixA[j][k] << endl;
+        }
+    }
+    cout << endl << "Matrix A: " << endl;
+    MatrixOutput(MatrixA);
+
+
+
+    for (j = 3; j < 4; j++)
+    {
+        n = 0;
+        while (MatrixA[j][n] == 0) {
+            n++;
+        }
+        buffer = MatrixA[j][n];
+        cout << endl << "buffer: " << buffer << endl;
+        for (int k = 0; k < MatrixDimension; k++)
+        {
+            MatrixA[j][k] -= MatrixA[2][k] * buffer / MatrixA[2][2];
         }
     }
     cout << endl << "Matrix A: " << endl;
@@ -57,7 +95,7 @@ int main()
     return 0;
 }
 
-void MatrixOutput(double Matrix[MatrixDimension][MatrixDimension]) 
+void MatrixOutput(float Matrix[MatrixDimension][MatrixDimension]) 
 {
     for (int i = 0; i < MatrixDimension; i++) {
         for (int j = 0; j < MatrixDimension; j++)
