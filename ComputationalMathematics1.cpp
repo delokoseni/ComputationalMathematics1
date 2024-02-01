61b94367a40aff8c34e3d00545db42b13d871d9d
delokoseni <107359196+delokoseni@users.noreply.github.com>
2024-02-01 15:53:56 +0700

    Подсчет Х

--- ComputationalMathematics1.cpp
+++ ComputationalMathematics1.cpp
@@ -1,15 +1,18 @@
 ﻿#include <iostream>
 #include <fstream>
+#include <Windows.h>
 
 const int MatrixDimension = 4;
-void MatrixOutput(float Matrix[MatrixDimension][MatrixDimension]);
-
+void MatrixOutput(float Matrix[MatrixDimension][MatrixDimension+1]);
+float GetDetermTriangMatrix(float Matrix[MatrixDimension][MatrixDimension + 1]);
 using namespace std;
 
 int main()
 {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     int i, j;
-    float MatrixA[MatrixDimension][MatrixDimension], MatrixB[MatrixDimension];
+    float Matrix[MatrixDimension][MatrixDimension+1];
     string FileName;
     /*cout << "Input name of file for matrix A: ";
     cin >> FileName;
@@ -18,29 +21,16 @@ int main()
     if (InputFileA.is_open())
     {
         for (i = 0; i < MatrixDimension; i++)
-            for (j = 0; j < MatrixDimension; j++)
-                InputFileA >> MatrixA[i][j];
+            for (j = 0; j < MatrixDimension+1; j++)
+                InputFileA >> Matrix[i][j];
         InputFileA.close();
     }
     else
-        cout << "Error! File for matrix A isn`t open!";
-    /*cout << "Input name of file for matrix B: ";
-    cin >> FileName;
-    ifstream InputFileB(FileName);*/
-    ifstream InputFileB("B.txt");
-    if (InputFileB.is_open())
-    {
-        for (i = 0; i < MatrixDimension; i++)
-            InputFileB >> MatrixB[i];
-        InputFileB.close();
-    }
-    else
-        cout << "Error! File for matrix B isn`t open!";
-    cout << endl << "Matrix A: " << endl;
-    MatrixOutput(MatrixA);
-    cout << endl << "Matrix B: " << endl;
-    for (i = 0; i < MatrixDimension; i++)
-        cout << MatrixB[i] << endl;
+        cout << "Ошибка! Не удалось открыть файл!";
+
+    cout << endl << "Матрица: " << endl;
+    MatrixOutput(Matrix);
+
     float buffer;
 
     int n = 0;
@@ -48,58 +38,83 @@ int main()
     for (j = 1; j < MatrixDimension; j++)
     {
         n = 0;
-        while (MatrixA[j][n] == 0) {
+        while (Matrix[j][n] == 0) {
             n++;
         }
-        buffer = MatrixA[j][n];
-        for (int k = 0; k < MatrixDimension; k++)
+        buffer = Matrix[j][n];
+        for (int k = 0; k < MatrixDimension+1; k++)
         {
-            MatrixA[j][k] -= MatrixA[0][k] * buffer / MatrixA[0][0];
+            Matrix[j][k] -= Matrix[0][k] * buffer / Matrix[0][0];
         }
     }
-    cout << endl << "Matrix A: " << endl;
-    MatrixOutput(MatrixA);
+    cout << endl << "Матрица: " << endl;
+    MatrixOutput(Matrix);
 
 
     for (j = 2; j < MatrixDimension; j++)
     {
-        buffer = MatrixA[j][1];
-        //cout << endl << "buffer[" << j << "][" << n << "]: " << buffer << endl;
-        for (int k = 0; k < MatrixDimension; k++)
+        buffer = Matrix[j][1];
+        for (int k = 0; k < MatrixDimension+1; k++)
         {
-            cout << MatrixA[j][k] << " - " << MatrixA[1][k] << " * " << buffer << " / " << MatrixA[1][1] << " = " << " ";
-            MatrixA[j][k] -= MatrixA[1][k] * buffer / MatrixA[1][1];
-            cout << MatrixA[j][k] << endl;
+            Matrix[j][k] -= Matrix[1][k] * buffer / Matrix[1][1];
         }
     }
-    cout << endl << "Matrix A: " << endl;
-    MatrixOutput(MatrixA);
+    cout << endl << "Матрица: " << endl;
+    MatrixOutput(Matrix);
 
 
 
     for (j = 3; j < 4; j++)
     {
         n = 0;
-        while (MatrixA[j][n] == 0) {
+        while (Matrix[j][n] == 0) {
             n++;
         }
-        buffer = MatrixA[j][n];
-        cout << endl << "buffer: " << buffer << endl;
-        for (int k = 0; k < MatrixDimension; k++)
+        buffer = Matrix[j][n];
+        for (int k = 0; k < MatrixDimension+1; k++)
         {
-            MatrixA[j][k] -= MatrixA[2][k] * buffer / MatrixA[2][2];
+            Matrix[j][k] -= Matrix[2][k] * buffer / Matrix[2][2];
         }
     }
-    cout << endl << "Matrix A: " << endl;
-    MatrixOutput(MatrixA);
+    cout << endl << "Матрица: " << endl;
+    MatrixOutput(Matrix);
+    cout << "Определитель матрицы: ";
+    cout << GetDetermTriangMatrix(Matrix) << endl;
+    if (GetDetermTriangMatrix(Matrix) == 0)
+        cout << "Матрица вырождена." << endl;
+    else
+    {
+        cout << "Матрица не вырождена." << endl;
+        float x1, x2, x3, x4;
+        x4 = Matrix[MatrixDimension - 1][MatrixDimension] / Matrix[MatrixDimension - 1][MatrixDimension - 1];
+        x3 = (Matrix[MatrixDimension - 2][MatrixDimension] - Matrix[MatrixDimension - 2][MatrixDimension - 1] * x4) /
+            Matrix[MatrixDimension - 2][MatrixDimension - 2];
+        x2 = (Matrix[MatrixDimension - 3][MatrixDimension] - Matrix[MatrixDimension - 3][MatrixDimension - 2] * x3
+            - Matrix[MatrixDimension - 3][MatrixDimension - 1] * x4) / Matrix[MatrixDimension - 3][MatrixDimension - 3];
+        x1 = (Matrix[MatrixDimension - 4][MatrixDimension] - Matrix[MatrixDimension - 4][MatrixDimension - 3] * x2 
+            - Matrix[MatrixDimension - 4][MatrixDimension - 2] * x3 - Matrix[MatrixDimension - 4][MatrixDimension - 1] * x4) /
+            Matrix[MatrixDimension - 4][MatrixDimension - 4];
+        cout << "X1 = " << x1 << endl;
+        cout << "X2 = " << x2 << endl;
+        cout << "X3 = " << x3 << endl;
+        cout << "X4 = " << x4 << endl;
+    }
     return 0;
 }
 
-void MatrixOutput(float Matrix[MatrixDimension][MatrixDimension]) 
+void MatrixOutput(float Matrix[MatrixDimension][MatrixDimension+1]) 
 {
     for (int i = 0; i < MatrixDimension; i++) {
-        for (int j = 0; j < MatrixDimension; j++)
-            cout << Matrix[i][j] << "\t";
+        for (int j = 0; j < MatrixDimension+1; j++)
+            cout << Matrix[i][j] << "\t\t";
         cout << endl;
     }
+}
+
+float GetDetermTriangMatrix(float Matrix[MatrixDimension][MatrixDimension + 1])
+{
+    float determinant = 1;
+    for (int i = 0; i < MatrixDimension; i++)
+        determinant *= Matrix[i][i];
+    return determinant;
 }
